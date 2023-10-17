cddfcd00d59bc18b5583d20d7c368125c74e493f
en0c-026 <bitup.games@gmail.com>
2023-10-17 18:02:14 +0200

    update: components index

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import StepForm from './StepForm.vue'
 import StepList from './StepList.vue'
 import CodeEditor from './CodeEditor.vue'
 import ScenarioManager from './ScenarioManager.vue'
+import ScenarioEditor from './ScenarioEditor.vue'
 import AccountForm from './AccountForm.vue'
 import TransactionForm from './TransactionForm.vue'
 import ExpectForm from './ExpectForm.vue'
@@ -28,4 +29,5 @@ export {
   LogForm,
   FileItem,
   FileList,
+  ScenarioEditor
 }
