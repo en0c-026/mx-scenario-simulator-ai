96602b0e90a797d15e8e953826831cc779667310
en0c-026 <bitup.games@gmail.com>
2023-10-17 18:08:19 +0200

    update: vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,15 +14,22 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   defaults: {
+    global: {
+      elevation: 0
+    },
     VSelect: {
-      variant: "solo-filled",
-      flat: true,
-      hideDetails: true
+      variant: "outlined",
+      rounded: 0,
+      
+      hideDetails: true,
+      density: "compact"
     },
     VTextField: {
-      variant: "solo-filled",
-      flat: true,
-      hideDetails: true
+      variant: "outlined",
+      rounded: 0,
+      
+      hideDetails: true,
+      density: "compact"
     },
     VBtn: {
       rounded: 0
