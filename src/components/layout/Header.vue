9a6e2317f6a30bfe321993c165cda4d343ed2d46
en0c-026 <bitup.games@gmail.com>
2023-10-19 23:24:54 +0200

    add logo to header and change bg color

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-app-bar color="warning">
+    <v-app-bar color="orange-darken-1">
       <template v-slot:prepend><v-app-bar-nav-icon v-if="store.projectPath && store.files.length > 0" variant="text" @click.stop="store.showSidenav = !store.showSidenav"></v-app-bar-nav-icon></template>
       <template v-slot:title>
-        <p class="font-weight-bold">ScenarioMx Studio</p>
+        <v-img src="/scenario-mx-studio-logo-2.png" width="280px"></v-img>
       </template>
     </v-app-bar>
 </template>
