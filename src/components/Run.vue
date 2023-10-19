6a0b1acf55ad5b9254757ef7f0c92bde1d7592f9
en0c-026 <bitup.games@gmail.com>
2023-10-19 13:18:12 +0200

    fix: console details

--- src/components/Run.vue
+++ src/components/Run.vue
@@ -5,16 +5,20 @@
     <v-btn elevation="1" @click="store.executeCommand('mxpy contract build', store.projectPath)">build</v-btn>
     <v-btn elevation="1" @click="store.executeCommand('mxpy contract test', store.projectPath)">test</v-btn>
   </v-sheet>
-  <div ref="consoleRef" @vnode-updated="scrollToBottom" class="bg-grey-darken-4 overflow-y-auto pt-2 px-4 pb-12 h-100">
-    <p v-for="log in store.logs" :class="getLogColor(log)">
-      {{ log }}
-    </p>
+  <div ref="consoleRef" class="bg-grey-darken-4 overflow-y-auto pt-2 px-4 pb-12 h-100">
+    <div v-for="log in store.logs">
+      <p v-if="log.startsWith('scenariomx-studio$')">
+        <span :class="getLogColor(log)">{{ splitCommand(log)[0] }}</span>
+        {{ splitCommand(log)[1] }}
+      </p>
+      <p v-else :class="getLogColor(log)">{{ log }}</p>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useAppStore } from '@/store/app';
-import { nextTick, ref } from 'vue';
+import { nextTick, ref, watch } from 'vue';
 const store = useAppStore();
 const consoleRef = ref(null)
 const getLogColor = (log) => {
@@ -25,14 +29,22 @@ const getLogColor = (log) => {
     logClass = 'text-blue-lighten-3 font-weight-bold'
   } else if (log.startsWith('PENDING')) {
     logClass = 'text-yellow-lighten-1 font-weight-bold'
+  } else if (log.startsWith('scenariomx-studio$')) {
+    logClass = 'text-light-green-accent-3 font-weight-bold'
   }
   return logClass
+}
+watch(store, () => {
+  scrollToBottom()
+})
+const splitCommand = (c) => {
+  const chunks = c.split('$')
+  return [`${chunks[0]}$`, chunks[1]]
 }
 const scrollToBottom = () => {
   nextTick(() => {
     const consoleElement = consoleRef.value;
     if (consoleElement) {
-      // Hacer scroll al final del elemento
       consoleElement.scrollTop = consoleElement.scrollHeight;
     }
   });
